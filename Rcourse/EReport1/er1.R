d07b3be5b1ae32ff3b735e67fd6218b1cdc6c913
holoprogram <2488070031@qq.com>
2022-09-27 10:08:57 +0800

    Experiment1 Report 1

--- Rcourse/EReport1/er1.R
+++ Rcourse/EReport1/er1.R
@@ -20,6 +20,15 @@ C <- A[1:3, 1:3]
 D <- B[,-3]
 
 
+
 ## question 4
-data <- read.table("er1data.txt", header = TRUE)
+read1 <- data.frame("姓名" = c("张三", "李四", "王五", "赵六", "丁一"),
+                    "性别" = factor(c("女", "男", "女", "男", "女" )),
+                    "年龄" = c(14, 15, 16, 14, 15),
+                    "身高" = c(156, 165, 157, 162, 159),
+                    "体重" = c(42.0, 49.0, 41.5, 52.0, 45.5))
+
+## question 5
+data <- read.table("er1data.txt", header = TRUE,
+                   colClasses = c("character","factor","numeric","numeric","numeric"))
 write.csv(data, "er1data.csv")
